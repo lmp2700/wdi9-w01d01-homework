21f46cd91ac93da75f4dd196edc6789a8081c21e
Liz Steak <lizmaepark@gmail.com>
2018-09-13 16:29:29 -0600

    Commit 1 - Verbal questions

---
+++ homework_part_4/app.js
@@ -0,0 +1,10 @@
+// 1. Verbal questions
+// What is the difference between a parameter and an argument?
+//     A parameter is a variable
+//     An argument is the data you pass through the parameter
+// Within a function, what is the difference between return and console.log?
+//     console.log will allow your completed data to show up in the javascript console (in Google Chrome)
+//     return will allow the information to be stored and can be accessed later if needed
+// What are the implications of the ability of a function to return a value?
+//     It allows the function to access the data further on in the code
+
