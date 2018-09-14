c6aa7db12582c6f2d6be9a00a6d4cb1ec7ddce85
Liz Steak <lizmaepark@gmail.com>
2018-09-13 19:33:02 -0600

    Commit 6 - Prime Numbers

--- homework_part_4/app.js
+++ homework_part_4/app.js
@@ -26,9 +26,67 @@
 // 3. Digit Sum
 // Write a function sumDigits that accepts a number and returns the sum of its digits.
 
-const sumDigits = (num) => {
-    return num.toString().split('').reduce(function (a, b) {
-        return parseInt(a) + parseInt(b);
-    })
+// const sumDigits = (num) => {
+//     return num.toString().split('').reduce(function (a, b) {
+//         return parseInt(a) + parseInt(b);
+//     })
+// }
+// console.log(sumDigits(421));
+
+// 4. Pythagoras
+// const calculateSide = (sideA, sideB) => {
+//     return sideC = Math.sqrt(sideA * sideA + sideB * sideB)
+// }
+// console.log(calculateSide(8, 6))
+// console.log(calculateSide(10, 14));
+
+// 5. Sum Array
+// const sumArray = (arr) => {
+//     let sum = 0; 
+//     for(i = 0; i < arr.length; i++){
+//     sum += arr[i];
+//     }
+//     return sum
+// }
+// console.log(sumArray([1, 2, 3, 4, 5, 6]));
+
+// 6. Prime Numbers
+// const checkPrime = (num) => {
+//     if(num < 2){ 
+//         return false;
+//     }
+//     for(var i = 2; i < num; i++) {
+//         if(num%i==0){
+//             return false;
+//     } else{
+//         return true;
+//         }
+//     }
+// }
+// console.log(checkPrime(97));
+
+const checkPrime = (num) => {
+    let prime = true;
+    for(let i = 2; i <= Math.sqrt(num); i++){
+        if(num % i === 0) {
+            prime = false;
+            break;}   
+    }
+    return prime && (num > 1);
 }
-console.log(sumDigits(421));
+//console.log(checkPrime(5));
+
+const printPrimes = (counter) => {
+    for (let counter = 1; counter <= 100; counter++) {
+        let notPrime = false;
+        for (let i = 2; i <= counter; i++) {
+            if (counter % i === 0 && i !== counter) {
+                notPrime = true;
+            }
+        }
+        if (notPrime === false) {
+                    console.log(counter);
+        }
+    }
+}
+console.log(printPrimes(97));
